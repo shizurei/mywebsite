0983782b3e770b736a7d8ed6c7ead94c659a8cd6
shizurei <saisabgajurel2@gmail.com>
2021-07-16 08:32:42 +0545

    image looking

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ function pokeAlert() {
     alert("don't poke me you dolt! (╯°□°）╯︵ ┻━┻");
 }
 
+function lookLeft() {
+    myPic.setAttribute("src", "images/lookLeft.jpg"); 
+}
+
+function lookLeftStop() {
+    myPic.setAttribute("src", "images/card.jpg");
+}
+
+function lookRight() {
+    myPic.setAttribute("src", "images/lookRight.jpg");
+}
+
+function lookRightStop() {
+    myPic.setAttribute("src", "images/card.jpg");
+}
