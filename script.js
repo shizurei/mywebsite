871381f80ee0841f7f1b729e09a8cfce68767e65
shizurei <saisabgajurel2@gmail.com>
2021-07-17 18:07:37 +0545

    animations wooo

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-function pokeAlert() {
-    alert("don't poke me you dolt! (╯°□°）╯︵ ┻━┻");
+//function to bloat up like a pufferfish when poked and provoked
+bigg = false;
+function poke() {
+    if(bigg == false) {
+        myPic.setAttribute("src", "images/cardBig.jpg");
+        bigg = true;
+        myPic.className = "wheel";
+    } else {
+        myPic.setAttribute("src", "images/cardNormal.jpg");
+        bigg = false;
+        myPic.className = "";
+    }
 }
 
-function lookLeft() {
-    myPic.setAttribute("src", "images/lookLeft.jpg"); 
-}
-
-function lookLeftStop() {
-    myPic.setAttribute("src", "images/cardNormal.jpg");
-}
-
-// function bigChild() {
-//     myPic.setAttribute("src", "images/cardBig.jpg");
-// }
-
-function smallChild() {
-    myPic.setAttribute("src", "images/cardNormal.jpg");
-}
-
-function lookRight() {
-    myPic.setAttribute("src", "images/lookRight.jpg");
-}
-
-function lookRightStop() {
-    myPic.setAttribute("src", "images/cardNormal.jpg");
+//fucnction to look left or right with eyes that stare and glare everywhere
+function look(stare) {
+    if(stare == "left") {
+        myPic.setAttribute("src", "images/lookLeft.jpg");
+    } else if(stare == "right") {
+        myPic.setAttribute("src", "images/lookRight.jpg");
+    } else if(stare == "off") {
+        if(bigg == true) {
+            myPic.setAttribute("src", "images/cardBig.jpg");
+        } else { 
+            myPic.setAttribute("src", "images/cardNormal.jpg");
+        }
+    }
 }
